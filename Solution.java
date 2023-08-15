545858b09987846588c206119c8228da3db396b5
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-08-14 17:00:22 -0700

    working on solution
    figured out how to return int[] when using List<Integer> as status

--- Solution.java
+++ Solution.java
@@ -1,5 +1,34 @@
+import java.util.*;
+
 public class Solution {
     public int[] asteroidCollision(int[] asteroids) {
-        return new int[]{};
+        List<Integer> status = new ArrayList<>();
+        for (int asteroid : asteroids) {
+            status.add(asteroid);
+        }
+        
+
+
+        return convertListToArray(status);
+    }
+
+    public int[] convertListToArray(List<Integer> list) {
+        int[] answer = new int[list.size()];
+        for (int index = 0; index < list.size(); index++) {
+            answer[index] = list.get(index);
+        }
+        return answer;
+    }
+
+    public void performStep(List<Integer> list) {
+        int index = 0;
+        int next = index + 1;
+        do {
+            
+            
+
+            index++;
+            next = index + 1;
+        } while (next < list.size());
     }
 }
